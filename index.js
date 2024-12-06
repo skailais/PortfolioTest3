030d7c367545dc69fd9972c05f2883d2dbfc5b8e
Pavel Kulbachnyi <77022646+skailais@users.noreply.github.com>
2024-12-06 14:46:30 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+function startLoader() {
+  let counterElement = document.querySelector(".counter");
+  let currentValue = 0;
+
+  function upgateCounter() {
+    if (currentValue === 100) {
+      return;
+    }
+
+    currentValue += Math.floor(Math.random() * 10) + 1;
+
+    if (currentValue > 100) {
+      currentValue = 100;
+    }
+
+    counterElement.textContent = currentValue;
+
+    let delay = Math.floor(Math.random() * 200) + 50;
+    setTimeout(upgateCounter, delay);
+  }
+
+  upgateCounter();
+}
+
+startLoader();
+
+gsap.to(".counter", 0.25, {
+  delay: 3.5,
+  opacity: 0,
+});
+
+gsap.to(".bar", 1.5, {
+  delay: 3.5,
+  height: 0,
+  stagger: {
+    amount: 0.5,
+  },
+  ease: "power4.inOut",
+});
+
 const $ = jQuery;
 // Button Variables
 const portfolioBtn = $(".portfolioBtn");
